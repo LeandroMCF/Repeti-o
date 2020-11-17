a4438bf556bbbf0da4c4347abd058dccec43ac43
Leandro Cavalcanti <leand@DESKTOP-LFIP8ID.(none)>
2020-11-17 11:59:11 -0300

    contando

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Repeti_o
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aprendendo WHILE");
+            int numero;
+            int contador = 0; 
+
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+            while (contador <= numero)
+            {
+                Console.WriteLine(contador);
+                contador = contador + 1;
+            }
+        }
+    }
+}
